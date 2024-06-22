01d3aced89e48e767a74330d4e142eee3df2598a
alalalsam <123228802+alalalsam@users.noreply.github.com>
2024-06-21 19:16:32 -0500

    more files

---
+++ loader/build/memory.d
@@ -0,0 +1,5 @@
+memory.o: C:/devkitPro/Luma3DS-master/sysmodules/loader/source/memory.c \
+ C:/devkitPro/Luma3DS-master/sysmodules/loader/source/memory.h \
+ C:/devkitPro/libctru/include/3ds/types.h
+C:/devkitPro/Luma3DS-master/sysmodules/loader/source/memory.h:
+C:/devkitPro/libctru/include/3ds/types.h:
